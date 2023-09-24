3914007f961d15402ba03d9a8d1e96fad2546187
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2023-09-24 10:22:55 +0800

    Update openssl to 3.1.3

--- build-x86.sh
+++ build-x86.sh
@@ -362,9 +362,9 @@ fi
 # build openssl
 # -----------------------------------------------------------------------------
 if [ ! -f "$INSTALL_PATH"/lib/libssl.a ]; then
-  wget -nc https://www.openssl.org/source/openssl-1.1.1w.tar.gz
-  tar -xf openssl-1.1.1w.tar.gz
-  cd openssl-1.1.1w || exit
+  wget -nc https://www.openssl.org/source/openssl-3.1.3.tar.gz
+  tar -xf openssl-3.1.3.tar.gz
+  cd openssl-3.1.3 || exit
   ./Configure \
   -m32 \
   --static \
